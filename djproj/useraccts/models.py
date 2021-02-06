49e33d18002871f1653ebf8ccf3e68f11a59f311
Shru Abram <mail@xzz.com>
2021-02-07 00:39:00 +0100

    changes to useraccts app

--- djproj/useraccts/models.py
+++ djproj/useraccts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from inventory.models import Destination
 
 # Create your models here.
+
+class Items(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=50)
+    price = models.IntegerField() 
+    quantity = models.IntegerField()
+    category = models.CharField(max_length=50)
--- djproj/useraccts/views.py
+++ djproj/useraccts/views.py
@@ -1,21 +1,72 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
+from django.contrib import messages
+from .models import Items
+from inventory.models import Destination
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'User not Found/Invalid Credentials')            
+    else:
+        return render(request,'login.html')
+       
 def register(request):
     if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name =  request.POST['last_name']
-        user_name = request.POST['user_name']
+        username = request.POST['username']
         email = request.POST['email']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
         
-        user = User.objects.create_user(first_name=first_name,last_name=last_name,
-               username=user_name,email=email,password=password1)
-        user.save()
-        print('user created')
-        return redirect('/')
-    else:  
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'User already exists')
+                print('user exists')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'Email already exits')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password1)
+                user.save()
+                messages.info(request,'User created')
+                print('user created')
+                return redirect('login')
+        else:
+            messages.info(request,'Password Mismatch')
+            return redirect('register')
+    else:
         return render(request,'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return render(request,'logout.html')
+
+def items(request):
+    if request.method == 'POST':
+        item_name = request.POST['item_name']
+        price = request.POST['price']
+        quantity = request.POST['quantity']
+        category = request.POST['category']
+
+        # item = Items(item_name=item_name, price=price,quantity=quantity,user_id=ID)
+        # item.save()
+        request.user.items_set.create(item_name=item_name, price=price,quantity=quantity,category=category)
+        return redirect('items')               
+    else:
+        items_ = Items.objects.all()
+        return render(request,'items.html',{'items_' : items_})  
+
+
+      
+
