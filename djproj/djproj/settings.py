b4504cf5322bb70cc29806da8f52b214a7138a17
Shru Abram <mail@xzz.com>
2021-02-07 00:23:17 +0100

    committing changes to settings.py

--- djproj/djproj/settings.py
+++ djproj/djproj/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'inventory.apps.InventoryConfig',
+    'useraccts.apps.UseracctsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
